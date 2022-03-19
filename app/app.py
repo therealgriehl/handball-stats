f73a956dcfef944f74e34cc68ef13d2b8e2966e5
robingri <r.griehl@hotmail.de>
2022-03-19 13:14:26 +0100

    better

--- app/app.py
+++ app/app.py
@@ -10,71 +10,20 @@ from pathlib import Path
 # --- Setup Streamlit ---
 st.set_page_config(layout="wide")
 
+
 #df_report = pd.read_csv(r"C:\Users\rgrie\PycharmProjects\handball-stats\data\220316_mHSOL_ab.csv", parse_dates=[3], dayfirst=True)
 
-df_report = pd.read_csv(r"https://raw.githubusercontent.com/therealgriehl/handball-stats/main/data/220316_mHSOL_ab.csv")
+df_report = pd.read_csv(r"https://raw.githubusercontent.com/therealgriehl/handball-stats/main/data/220316_mHSOL_ab.csv", parse_dates=[3], dayfirst=True)
 
 all_games = df_report["game_nr"].unique().tolist()
 all_teams = np.unique(df_report[["home", "guest", "team"]].values)
 all_players = df_report["player"].unique().tolist()
 
-st.title("Hello World")
-
-st.header("DataFrame")
-st.dataframe(df_report)
-
-left_column, right_column = st.columns(2)
-
-left_column.header("Tore pro Team")
-teamgoals = df_report.groupby(["team"]).sum().sort_values(by="goal", ascending=False)
-left_column.bar_chart(teamgoals["goal"])
-
-right_column.header("2min pro Team")
-team_2min = df_report.groupby(["team"]).sum().sort_values(by="2min", ascending=False)
-right_column.bar_chart(team_2min["2min"])
-
-# --- Geworfene Tore ---
-st.header("Top Scorer")
-player_goals = df_report.groupby("player").sum().sort_values(by="goal", ascending=False)
-top_goals = player_goals.head(10)
-top_goals_players = top_goals.index.values.tolist()
-st.bar_chart(top_goals["goal"])
-
-# --- Geworfene Tore in der Saison ---
-
-st.subheader("Top Scorer im Saisonverlauf")
-time_sorted_df = df_report.sort_values(by=["game_date", "game_nr"], ascending=True)
-time_sorted_df["sum_goals"] = time_sorted_df.groupby("player")["goal"].transform(pd.Series.cumsum)
-date_index = time_sorted_df.groupby(["game_date", "player"]).max()
-date_index = date_index.reset_index()
-top_date_index = date_index[date_index["player"].isin(top_goals_players)]
-
-fig_px = px.line(top_date_index, x="game_date", y="sum_goals", color="player", markers=True)
-st.plotly_chart(fig_px, use_container_width=True)
-
-# Player Selection
-st.subheader("Vergleiche den Torverlauf von zwei Spielern:")
-option_player1 = st.selectbox(
-    'Spieler 1:',
-     all_players)
-option_player2 = st.selectbox(
-    'Spieler 2:',
-     all_players)
-
-option_players = [option_player1, option_player2]
-
-selection = date_index[date_index["player"].isin(option_players)]
-fig = plt.figure(figsize=(15,6))
-sns.lineplot(data=selection, x="game_date", y="sum_goals", hue="player", marker='o',linestyle="--")
-plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
-plt.xlabel("Datum")
-plt.ylabel("Tore")
-st.pyplot(fig)
-
+st.title("Handball Dashboard")
 
 # --- Teams im Saisonverlauf ---
-st.header("Teams im Saisonverlauf")
-games_df = time_sorted_df.groupby(["game_nr", "team"])["goal"].sum()
+
+games_df = df_report.groupby(["game_nr", "team"])["goal"].sum()
 games_df = games_df.reset_index()
 
 wins = []
@@ -89,7 +38,7 @@ for game in all_games:
 
 wins_df = pd.DataFrame(wins, columns=["game_nr", "winner"])
 
-merged_df = pd.merge(time_sorted_df, wins_df, on="game_nr", how="inner")
+merged_df = pd.merge(df_report, wins_df, on="game_nr", how="inner")
 
 points = merged_df.groupby(["game_nr"])["winner", "home", "guest", "game_date"].max()
 points = points.reset_index()
@@ -110,11 +59,155 @@ points_df = points_df.sort_values(by="game_date")
 points_df["sum_points+"] = points_df.groupby("team")["points+"].transform(pd.Series.cumsum)
 points_df["sum_points-"] = points_df.groupby("team")["points-"].transform(pd.Series.cumsum)
 
-fig2 = plt.figure(figsize=(15,8))
-sns.lineplot(data=points_df, x="game_date", y="sum_points+", hue="team", marker='o',linestyle="--")
-plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
-st.pyplot(fig2)
+amount_games = points_df.groupby("team").count()
+amount_games = amount_games.reset_index()
 
+amount_games= amount_games.drop(columns=["game_date", "points+", "points-", "sum_points+", "sum_points-"])
+amount_games.rename(columns={"game_nr":"amount_games"}, inplace=True)
+
+fig_teams = px.line(points_df,
+                    x="game_date",
+                    y="sum_points+",
+                    color="team",
+                    markers=True,
+                    title="Punkte im Saisonverlauf",
+                    labels={
+                        "game_date":"Datum",
+                        "sum_points+":"Anzahl Punkte"
+                    })
+fig_teams.update_yaxes(tick0=0, dtick=2)
+fig_teams.update_layout(legend_title_text='Mannschaften')
+st.plotly_chart(fig_teams, use_container_width=True)
+
+
+
+
+
+# --- Tore pro Team ---
+
+teamgoals = df_report.groupby(["team"]).sum().sort_values(by="goal", ascending=False)
+teamgoals = teamgoals.reset_index()
+teamgoals = pd.merge(teamgoals, amount_games, on="team", how="inner")
+left_column, right_column = st.columns(2)
+
+fig_tt = px.bar(teamgoals, x="team", y="goal",
+                   title="Tore pro Team",
+                   labels={
+                       "team":"Mannschaft",
+                       "goal":"Geworfene Tore"
+                   })
+fig_tt.update_yaxes(tick0=0, dtick=50)
+left_column.plotly_chart(fig_tt)
+
+# --- Tore pro Spiel - Teams
+
+def get_per_team(row):
+    return row["goal"]/(row["amount_games"])
+teamgoals["goals_pg"] = teamgoals.apply(lambda x: get_per_team(x), axis=1)
+teamgoals_sort = teamgoals.sort_values(by="goals_pg", ascending=False)
+fig_tt2 = px.bar(teamgoals_sort, x="team", y="goals_pg",
+                   title="Tore pro Spiel",
+                   labels={
+                       "team":"Mannschaft",
+                       "goals_pg":"Tore pro Spiel"
+                   })
+fig_tt2.update_yaxes(tick0=0, dtick=10)
+right_column.plotly_chart(fig_tt2)
+
+# --- 2min pro Team ---
+
+team_2min = df_report.groupby(["team"]).sum().sort_values(by="2min", ascending=False)
+team_2min = team_2min.reset_index()
+
+fig_t2 = px.bar(team_2min, x="team", y="2min",
+                   title="2-min pro Team",
+                   labels={
+                       "team":"Mannschaft",
+                       "2min":"Zeitstrafen"
+                   })
+fig_t2.update_yaxes(tick0=0, dtick=5)
+left_column.plotly_chart(fig_t2)
+
+# --- Prozent in Unterzahl ---
+
+team_2min["Zeit2min"] = team_2min["2min"].apply(lambda x: x*2)
+team_2min = pd.merge(team_2min, amount_games, on="team", how="inner")
+
+def get_per(row):
+    return (row["Zeit2min"]/(row["amount_games"]*60))*100
+team_2min["percent_2min"] = team_2min.apply(lambda x: get_per(x), axis=1)
+team_2min_sort = team_2min.sort_values(by="percent_2min", ascending=False)
+fig_t22 = px.bar(team_2min_sort, x="team", y="percent_2min",
+                   title="Verbrachte Zeit in Unterzahl",
+                   labels={
+                       "team":"Mannschaft",
+                       "percent_2min":"Zeit in Unterzahl in %"
+                   })
+fig_t22.update_yaxes(tick0=0, dtick=10)
+right_column.plotly_chart(fig_t22)
+
+# --- Geworfene Tore ---
+
+player_goals = df_report.groupby("player").sum().sort_values(by="goal", ascending=False)
+top_goals = player_goals.head(10)
+top_goals = top_goals.reset_index()
+top_goals_players = top_goals["player"].values.tolist()
+
+fig_score = px.bar(top_goals, x="player", y="goal",
+                   title="Top Scorer der Saison",
+                   labels={
+                       "player":"Spieler",
+                       "goal":"Geworfene Tore"
+                   })
+fig_score.update_yaxes(tick0=0, dtick=5)
+st.plotly_chart(fig_score)
+
+# --- Tore pro Spiel - Spieler
+right_column.subheader("Prüfe, wann Spieler gespielt")
+
+# --- Geworfene Tore in der Saison ---
+
+time_sorted_df = df_report.sort_values(by=["game_date", "game_nr"], ascending=True)
+time_sorted_df["sum_goals"] = time_sorted_df.groupby("player")["goal"].transform(pd.Series.cumsum)
+date_index = time_sorted_df.groupby(["game_date", "player"]).max()
+date_index = date_index.reset_index()
+top_date_index = date_index[date_index["player"].isin(top_goals_players)]
+fig_px = px.line(top_date_index, x="game_date",
+                 y="sum_goals", color="player",
+                 markers=True,
+                 title="Tore im Saisonverlauf",
+                    labels={
+                        "game_date":"Datum",
+                        "sum_goals":"Anzahl Tore"
+                    })
+fig_px.update_yaxes(tick0=0, dtick=5)
+fig_px.update_layout(legend_title_text='Spieler')
+st.plotly_chart(fig_px, use_container_width=True)
+
+# Player Selection
+st.subheader("Vergleiche den Torverlauf von zwei Spielern:")
+option_player1 = st.selectbox(
+    'Spieler 1:',
+     all_players)
+option_player2 = st.selectbox(
+    'Spieler 2:',
+     all_players)
+
+option_players = [option_player1, option_player2]
+
+selection = date_index[date_index["player"].isin(option_players)]
+
+fig_pc = px.line(selection, x="game_date",
+                 y="sum_goals", color="player",
+                 markers=True,
+                 title="Tore im Saisonverlauf",
+                    labels={
+                        "game_date":"Datum",
+                        "sum_goals":"Anzahl Tore"
+                    })
+fig_pc.update_yaxes(tick0=0, dtick=5)
+fig_pc.update_layout(legend_title_text='Spieler')
+st.plotly_chart(fig_pc, use_container_width=True)
 
 
 
@@ -124,6 +217,58 @@ st.pyplot(fig2)
 st.caption(
     " Vielen Dank für den Besuch!"
 )
+
+st.write(
+             """
+**Impressum**
+Angaben gemäß § 5 TMG
+
+Max Muster
+Musterweg
+12345 Musterstadt
+
+Vertreten durch:
+Max Muster
+
+Kontakt:
+Telefon: 01234-789456
+Fax: 1234-56789
+E-Mail: max@muster.de
+
+Umsatzsteuer-ID:
+Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: Musterustid.
+
+Wirtschafts-ID:
+Musterwirtschaftsid
+
+Aufsichtsbehörde:
+Musteraufsicht Musterstadt
+
+Haftungsausschluss:
+
+Haftung für Inhalte
+
+Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+
+Urheberrecht
+
+Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+
+Datenschutz
+
+Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+
+
+Google AdSense
+
+Diese Website benutzt Google Adsense, einen Webanzeigendienst der Google Inc., USA (''Google''). Google Adsense verwendet sog. ''Cookies'' (Textdateien), die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Google Adsense verwendet auch sog. ''Web Beacons'' (kleine unsichtbare Grafiken) zur Sammlung von Informationen. Durch die Verwendung des Web Beacons können einfache Aktionen wie der Besucherverkehr auf der Webseite aufgezeichnet und gesammelt werden. Die durch den Cookie und/oder Web Beacon erzeugten Informationen über Ihre Benutzung dieser Website (einschließlich Ihrer IP-Adresse) werden an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website im Hinblick auf die Anzeigen auszuwerten, um Reports über die Websiteaktivitäten und Anzeigen für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen. Das Speichern von Cookies auf Ihrer Festplatte und die Anzeige von Web Beacons können Sie verhindern, indem Sie in Ihren Browser-Einstellungen ''keine Cookies akzeptieren'' wählen (Im MS Internet-Explorer unter ''Extras > Internetoptionen > Datenschutz > Einstellung''; im Firefox unter ''Extras > Einstellungen > Datenschutz > Cookies''); wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.
+
+Impressum vom Impressum Generator der Kanzlei Hasselbach, Frankfurt
+
+             
+             """)
 
 
 
